7d7320c06b11ed10ded3fc0e505fee288a8aee67
Computer channel <Computer channel@AYMAN>
2024-11-06 23:57:48 +0500

    Add project files.

---
+++ craveyard/Server/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace craveyard.Server.Repositories
+{
+    public interface IUserRepository
+    {
+        int RegisterUser(User user); // Synchronous version, returns int directly
+        User LoginUser(string username, string password); // Synchronous version, returns User directly
+    }
+}
---
+++ craveyard/Server/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using craveyard.Server.Repositories;
+using System.Data;
+using Dapper;
+
+namespace craveyard.Server.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDbConnection _db;
+
+        public UserRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public int RegisterUser(User user)
+        {
+            var sql = "INSERT INTO Users (Username, Password, Email) VALUES (@Username, @Password, @Email)";
+            return _db.Execute(sql, user); // Synchronous execution
+        }
+
+        public User LoginUser(string username, string password)
+        {
+            var sql = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password";
+            return _db.QueryFirstOrDefault<User>(sql, new { Username = username, Password = password }); // Synchronous execution
+        }
+    }
+}
